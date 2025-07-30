7241196daf52e76154d6cd9f8cec653a944e4b01
tuanttdev <tuantt01.dev@gmail.com>
2025-07-31 01:16:52 +0700

    update task etl for dim_customer, dim_product

--- mongodb/customers.py
+++ mongodb/customers.py
@@ -6,8 +6,16 @@ import pandas as pd
 from clickhouse_connect import *
 from mongodb.click_house_connector import *
 from datetime import datetime
+import time
+from dotenv import load_dotenv
+
+load_dotenv()
+os.environ['TZ'] = os.getenv('TZ')
+time.tzset()
+
 COLLECTION_NAME = "customer"
 DIM_TABLE_NAME = "dim_customer"
+
 
 def extract_collection(data_interval_start=None, data_interval_end=None, full_load = False):
     ## get customer data from mongo collection
@@ -21,6 +29,10 @@ def extract_collection(data_interval_start=None, data_interval_end=None, full_lo
         data = collection.find()
     else:
         ## incremental load
+
+        data_interval_start = data_interval_start.strftime('%Y-%m-%d %H:%M:%S')
+        data_interval_end = data_interval_end.strftime('%Y-%m-%d %H:%M:%S')
+
         print(f"ETL từ {data_interval_start} đến {data_interval_end}")
         data = collection.find({"update_date": {"$gte": data_interval_start, "$lt": data_interval_end}})
 
@@ -41,40 +53,29 @@ def convert_collection_to_df(customer_data):
 
     cus_list = list(customer_data)
     customer_df  = pd.DataFrame(cus_list) ##
-    ## transformation
-    ## remove column
-    customer_df = customer_df[['_id' , 'first_name' , 'last_name', 'full_name', 'dob', 'ward_id', 'createdAt']]
-    customer_df.rename({'createdAt': 'created_at', '_id':'customer_id' }, axis=1, inplace=True)
+    if not(customer_df.empty) :
+        ## transformation
+        ## remove column
+
+        customer_df = customer_df[['_id' , 'first_name' , 'last_name', 'full_name', 'dob', 'ward_id', 'createdAt']]
+        customer_df.rename({'createdAt': 'created_at', '_id':'customer_id' }, axis=1, inplace=True)
 
 
-    customer_df['customer_id'] = customer_df['customer_id'].astype(str)
-    customer_df['ward_id'] = customer_df['ward_id'].astype(str)
-    #
-    customer_df['dob'] = customer_df['dob'].apply(fix_dob)
+        customer_df['customer_id'] = customer_df['customer_id'].astype(str)
+        customer_df['ward_id'] = customer_df['ward_id'].astype(str)
+        #
+        customer_df['dob'] = customer_df['dob'].apply(fix_dob)
 
-    customer_df['dob'] = customer_df['dob'].where(pd.notnull(customer_df['dob']), None)
+        customer_df['dob'] = customer_df['dob'].where(pd.notnull(customer_df['dob']), None)
 
 
-    ## Convert dob from str to datetime.date
-    #
-    # customer_df['dob'] = pd.to_datetime(
-    #     customer_df['dob'],
-    #     # format='%d/%m/%Y',
-    #     format='mixed',
-    #     dayfirst=True,
-    #     # errors='coerce'
-    # )
-
-    customer_df['created_at'] = pd.to_datetime(
-        customer_df['created_at'],
-        # format='%d/%m/%Y',
-        format='mixed',
-        dayfirst=True,
-        # errors='coerce'
-    )
-    #
-    # customer_df['dob'] = customer_df['dob'].where(pd.notnull(customer_df['dob']), None)
-    # customer_df['dob'] = customer_df['dob'].fillna(pd.Timestamp('1900-01-01').date())
+        customer_df['created_at'] = pd.to_datetime(
+            customer_df['created_at'],
+            # format='%d/%m/%Y',
+            format='mixed',
+            dayfirst=True,
+            # errors='coerce'
+        )
 
     return customer_df
 
@@ -89,20 +90,32 @@ def send_dataframe_to_clickhouse(data_interval_start=None, data_interval_end=Non
     if len(rows) == 0:
         full_load = True
     else:
-        last_time_update = rows[0]['last_update']
+        print(rows)
+        last_time_update = rows[0][1]
+    print(f'data_interval_start{data_interval_start}' )
+    print(f'last_time_update {last_time_update}')
 
     data = extract_collection(data_interval_start=data_interval_start, data_interval_end=data_interval_end, full_load = full_load)
     customer_df = convert_collection_to_df(data)
+    print(customer_df)
+    if not customer_df.empty:
+        rows = customer_df.values.tolist()
 
-    rows = customer_df.values.tolist()
+        clickhouse_client.insert(
+            table=DIM_TABLE_NAME,
+            data=rows,
+            column_names=customer_df.columns.tolist()
+        )
 
-    clickhouse_client.insert(
-        table='retailDataWareHouse.dim_customer',
-        data=rows,
-        column_names=customer_df.columns.tolist()
-    )
-    print(f'Load {len(rows)} rows into ClickHouse successfully')
+        print(f'Load {len(rows)} rows into ClickHouse successfully')
+    else:
+        print(f'No new data')
+
+    update_table_info(DIM_TABLE_NAME)
+
     return True
 
 if __name__ == '__main__':
-    send_dataframe_to_clickhouse()
+    send_dataframe_to_clickhouse(data_interval_start=datetime.strptime('2025-07-28 07:40:00','%Y-%m-%d %H:%M:%S') , data_interval_end=datetime.strptime('2025-07-28 07:40:00', '%Y-%m-%d %H:%M:%S'))
+    print(datetime.now())
+    # extract_collection()
